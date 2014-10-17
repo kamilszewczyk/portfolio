1da928217576d7f32d86d7497ff645c048efd11f
Kamil Szewczyk <szewczyk.kamil@gmail.com>
2014-10-17 22:48:11 +0100

    Fixed showing polish glyphs, working on correclty showing projects

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -38,6 +38,171 @@ angular.module( 'portfolioApp.home', [
  * And of course we define a controller for our route.
  */
 .controller( 'HomeCtrl', function HomeController( $scope ) {
+    $scope.projects = [
+        {
+            'id': 'artintheage',
+            'name': 'Art in the Age',
+            'img': 'artintheage.jpg',
+            'url': 'http://www.artintheage.com/'
+        },
+        {
+            'id': 'my-asho',
+            'name': 'My Asho',
+            'img': 'asho.jpg',
+            'url': 'http://www.myasho.com/'
+        },
+        {
+            'id': 'audiogo',
+            'name': 'AudioGo',
+            'img': 'audiogo.jpg'
+        },
+        {
+            'id': 'autopilot',
+            'name': 'Autopilot',
+            'img': 'autopilot.jpg'
+        },
+        {
+            'id': 'bijougraphie',
+            'name': 'Bijougraphie',
+            'img': 'bijougraphie.jpg',
+            'url': 'http://bijougraphie.com/'
+        },
+        {
+            'id': 'bike-swanky',
+            'name': 'Bike Swanky',
+            'img': 'bikeswanky.jpg',
+            'ult': 'https://www.bikeswanky.co.uk/'
+        },
+        {
+            'id': 'bonobos',
+            'name': 'Bonobos',
+            'img': 'bonobos.jpg',
+            'url': 'http://www.bonobos.com/'
+        },
+        {
+            'id': 'call-center-school',
+            'name': 'Call Center School',
+            'img': 'callcenterschool.jpg',
+            'url': 'https://www.thecallcenterschool.com/'
+        },
+        {
+            'id': 'caws',
+            'name': 'CAWS',
+            'img': 'caws.jpg',
+            'url': 'http://caws.ca/'
+        },
+        {
+            'id': 'cienen',
+            'name': 'Cienen',
+            'img': 'cienen.jpg',
+            'url': 'http://www.cienen.com/'
+        },
+        {
+            'id': 'cool-material',
+            'name': 'Cool Material',
+            'img': 'coolmaterial.png'
+        },
+        {
+            'id': 'dealer-direct-parts',
+            'name': 'Dealer Direct Parts',
+            'img': 'dealerdirectparts.png'
+        },
+        {
+            'id': 'eternal-snow',
+            'name': 'Eternal Snow',
+            'img': 'eternalsnow.png'
+        },
+        {
+            'id': 'ferracci',
+            'name': 'Ferracci',
+            'img': 'ferracci.png'
+        },
+        {
+            'id': 'homeq',
+            'name': 'Homeq',
+            'img': 'homeq.jpg'
+        },
+        {
+            'id': 'interbaden',
+            'name': 'Interbaden',
+            'img': 'interbaden.png'
+        },
+        {
+            'id': 'kidsongs',
+            'name': 'Kidsongs',
+            'img': 'kidsongs.png'
+        },
+        {
+            'id': 'listrak',
+            'name': 'Listrak',
+            'img': 'listrak.png'
+        },
+        {
+            'id': 'luggage-online',
+            'name': 'Luggage Online',
+            'img': 'luggageonline.png'
+        },
+        {
+            'id': 'nautical-cross',
+            'name': 'Nautical Cross',
+            'img': 'nauticalcross.jpg'
+        },
+        {
+            'id': 'roku',
+            'name': 'Roku',
+            'img': 'roku.jpg'
+        },
+        {
+            'id': 'sailor-jerry',
+            'name': 'Sailor Jerry',
+            'img': 'sailorjerry.jpg'
+        },
+        {
+            'id': 'salespro',
+            'name': 'SalesPro',
+            'img': 'salespro.png'
+        },
+        {
+            'id': 'salonmonster',
+            'name': 'SalonMonster',
+            'img': 'salonmonster.png'
+        },
+        {
+            'id': 'smart-earth',
+            'name': 'Smart Earth',
+            'img': 'smartearth.jpg'
+        },
+        {
+            'id': 'sourpuss',
+            'name': 'Sourpuss',
+            'img': 'sourpuss.png'
+        },
+        {
+            'id': 'sutherland-consulting',
+            'name': 'Sutherland Consulting',
+            'img': 'sutherlandconsulting.png'
+        },
+        {
+            'id': 'toddle',
+            'name': 'Toddle',
+            'img': 'toddle.png'
+        },
+        {
+            'id': 'tom-shiffman-photo',
+            'name': 'Tom Shiffman Photograpgy',
+            'img': 'tomshiffmanphoto.jpg'
+        },
+        {
+            'id': 'wise-bites',
+            'name': 'Wise Bites',
+            'img': 'wise-bites.jpg'
+        },
+        {
+            'id': 'youth-juice',
+            'name': 'Youth Juice',
+            'img': 'youthjuice.png'
+        }
+    ];
 })
 
 ;
