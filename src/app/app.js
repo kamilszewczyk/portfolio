bd2902fc71daf804927b5f5f752ffad674412da2
Kamil Szewczyk <szewczyk.kamil@gmail.com>
2015-01-18 22:49:56 +0000

    mobile menu working

--- src/app/app.js
+++ src/app/app.js
@@ -17,13 +17,17 @@ angular.module('portfolioApp', [
 .run( function run () {
 })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-        $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-                if ( angular.isDefined( toState.data.pageTitle ) ) {
-                        $scope.pageTitle = toState.data.pageTitle + ' | Kamil Szewczyk Magento Certified Developer' ;
-                }
-        });
+.controller( 'AppCtrl', function AppCtrl ( $scope, $window ) {
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+        if ( angular.isDefined( toState.data.pageTitle ) ) {
+            $scope.pageTitle = toState.data.pageTitle + ' | Kamil Szewczyk Magento Certified Developer' ;
+        }
+    });
+    if ($window.outerWidth > 900) {
+        $scope.toggle = true;
+    } else {
+        $scope.toggle = false;
+    }
 })
-
 ;
 
